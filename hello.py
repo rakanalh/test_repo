e3a891805a38cb082223a4ecb20ad0f5d68f7e06
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2017-05-05 17:58:50 +0200

    Hello

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
-Hello world 1
-hello world 2
-Hello world 3
-Hello world 4
-Hello world 5
-Hello world 6
-Hello world 7
+def hello():
+    Hello world 1
+    hello world 2
+    Hello world 3
+    Hello world 4
+    Hello world 5
+    Hello world 6
+    Hello world 7
+    Hello world 8
